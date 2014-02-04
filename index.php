bff4ece61628dcf239b704ac900ff65e0806d192
Pieplu <alexis.pieplu@gmail.com>
2014-02-03 19:25:05 -0500

    fix grid problems (margin not include)

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 <div class="main wrapper clearfix">
-	<div class="w75 left">
+	<div class="grid3-1"><div class="">
 	  <?php get_template_part('loop'); ?>
 	  <?php previous_posts_link(); ?>
 	  <?php next_posts_link(); ?>
 	  </div>
-	<div class="w20 Pml5 left">
+	<div class="">
 	  <?php get_sidebar(); ?>
-    </div>
+    </div></div>
 </div>
 <?php get_footer(); ?>
 <?php $points= round( 35+(Prix-35)*0.75*exp(-4.16*pow(10,-4)*Prix)); ?>
